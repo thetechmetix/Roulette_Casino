a1958dd237947bff939aa28b76207ac00f0a8d9c
techmetix <techmetix@gmail.com>
2019-08-29 00:11:14 +0530

    Added DB PORT in .env file

--- config/dbconnection.js
+++ config/dbconnection.js
@@ -4,6 +4,7 @@ var connection = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
     insecureAuth: true,
     multipleStatements: true
 });
